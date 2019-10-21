e3d774f0ca782eaeacb397d74e29fb9ba283384a
ahmedpasic <apasic2@etf.unsa.ba>
2019-10-21 16:42:46 +0200

    implementirana klasa Artikl

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String naziv;
+    private double cijena;
+    private String kod;
+
+    public Artikl(String n, double c, String k) {
+        naziv = n;
+        cijena = c;
+        kod = k;
+    }
 }
--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,8 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+
+    private static Artikl[] nizArtikala = new Artikl[] (50);
+
+
 }
